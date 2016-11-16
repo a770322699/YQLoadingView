3c51e66354249dbf58eadf8bc8bde6581df78449
YiQuan <770322699@qq.com>
2016-11-16 16:32:07 +0800

    init

---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by maygolf on 16/11/16.
+//  Copyright © 2016年 yiquan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
